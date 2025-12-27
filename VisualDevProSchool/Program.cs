6bec381a03b702e92f7958f591a807a3ea4cdeb1
jagui1 <agui1@umbc.edu>
2025-12-26 22:09:18 -0600

    Adds database

--- VisualDevProSchool/Program.cs
+++ VisualDevProSchool/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Cruel World!");
