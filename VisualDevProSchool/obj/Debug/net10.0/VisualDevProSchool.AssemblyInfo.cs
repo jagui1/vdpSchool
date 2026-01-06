0139edfc4c334c54c64b44bd0061fd47c207d837
jagui1 <agui1@umbc.edu>
2026-01-05 23:52:17 -0600

    Adds frontend

--- VisualDevProSchool/obj/Debug/net10.0/VisualDevProSchool.AssemblyInfo.cs
+++ VisualDevProSchool/obj/Debug/net10.0/VisualDevProSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualDevProSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0ec17c698399e3f22b3a4b28e6caf7672c3d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504de175ebb537e78756940f34ad5c5f6ec3b2de")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualDevProSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualDevProSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
